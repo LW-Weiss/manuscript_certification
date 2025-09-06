270cb273a29b15ed69ab14c8043a564f094f8394
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-09-06 18:03:17 -0300

    fixed spelling

--- corpora/armanc_scientific_papers/scientific_papers.py
+++ corpora/armanc_scientific_papers/scientific_papers.py
@@ -43,8 +43,8 @@ Scientific papers datasets contains two sets of long and structured documents.
 The datasets are obtained from ArXiv and PubMed OpenAccess repositories.
 
 Both "arxiv" and "pubmed" have two features:
-  - article: the body of the document, pagragraphs seperated by "/n".
-  - abstract: the abstract of the document, pagragraphs seperated by "/n".
+  - article: the body of the document, paragraphs seperated by "/n".
+  - abstract: the abstract of the document, paragraphs seperated by "/n".
   - section_names: titles of sections, seperated by "/n".
 
 """
